6b9b5f05f78dad32bc9dba4e217dfd7a75a1d3fe
j.jmonsalveg@gmail.com <j.jmonsalveg@gmail.com@baf7fb64-1a94-4593-70a7-5fc2ac22f43f>
2012-03-11 20:18:16 +0000

    proyecto casado con interfaz graficas falta. 1. eliminar conexinones terminadas 2. que exista una lista de paquetes para sacarle informacion en la parte grafica 3.caso no soportado2 4.que el paquete tenga un campo que sea paquete al que acepta 5.ver como añadir archivos al proyecto 6.levantar imagen 7.mejorar informacion de interfaz

---
+++ ProyectoRedes/pcapthread.h
@@ -0,0 +1,61 @@
+#ifndef PCAPTHREAD_H
+#define PCAPTHREAD_H
+
+#include <QThread>
+#include <struct.h>
+#include <QHash>
+#include <conexion.h>
+#include <fstream>
+#include <QDebug>
+#include <QMutex>
+#include <tablepacket.h>
+
+#define LOOP_FOREVER -1
+
+using namespace std;
+
+typedef struct interface {
+    char *name;
+    int datalink_type;
+} Interface;
+
+class PcapThread : public QThread
+{
+    Q_OBJECT
+
+    char errbuf[PCAP_ERRBUF_SIZE];
+    bpf_u_int32 net;
+    struct bpf_program fp;
+    QString interfaceName;
+
+
+public:
+
+    pcap_t * handler;
+    bool started;
+
+    explicit PcapThread();
+    QString getInterfaceName();
+    QList<Interface> getInterfaces();
+
+    bool initInterface();
+    std::string getPcapError();
+    void resetValues();
+
+    void setTablePacket(TablePacket *&);
+
+    /* Thread Functions */
+    void run();
+    ~PcapThread();
+
+private:
+    bool checkDevice();
+    bool openDevice();
+    bool compileFilter();
+    bool execFilter();
+
+public slots:
+    void setInterfaceName(QString interfaceName);
+};
+
+#endif // PCAPTHREAD_H
