6683b5b0b87f4136b958ba1a32c2d9cda591eaa5
ajclopez9@gmail.com <ajclopez9@gmail.com@baf7fb64-1a94-4593-70a7-5fc2ac22f43f>
2012-03-12 16:20:30 +0000

    Arreglado Banderas!

--- trunk/ProyectoRedes/textpacket.cpp
+++ trunk/ProyectoRedes/textpacket.cpp
@@ -27,19 +27,18 @@ void TextPacket::setRowPackect(int row, int col)
     QString seqNextNumber = "[ Siguiente Número de Secuencia: " + QString::number(listaPacket[row].getNextSeq()) + " ]<br>";
     QString ackNumber = "Número de Acknowledgement: " + QString::number(listaPacket[row].getNumAck()) + "<br>";
     QString flags = "Banderas: <br>";
-    QString cwr = "      "  + QString::number(listaPacket[row].getCWR()) + " = " + "Reducir Ventana Congestion <br>";
-    QString ece = "      "  + QString::number(listaPacket[row].getECE()) + " = " + "ECN-Echo <br>";
-    QString urg = "      "  + QString::number(listaPacket[row].getURG()) + " = " + "Urgente <br>";
-    QString ack = "      "  + QString::number(listaPacket[row].getACK()) + " = " + "Acknowledgement <br>";
-    QString psh = "      "  + QString::number(listaPacket[row].getPUSH()) + " = " + "Empuje <br>";
-    QString rst = "      "  + QString::number(listaPacket[row].getRST()) + " = " + "Reajustar <br>";
-    QString syn = "      "  + QString::number(listaPacket[row].getSYN()) + " = " + "Syn <br>";
-    QString fin = "      "  + QString::number(listaPacket[row].getFIN()) + " = " + "Fin <br>";
+    QString cwr = "      "  + QString::number(listaPacket[row].getCWR()) + " = " + "CWR (Congestion Window Reduced) <br>";
+    QString ece = "      "  + QString::number(listaPacket[row].getECE()) + " = " + "ECE (ECN-Echo) <br>";
+    QString urg = "      "  + QString::number(listaPacket[row].getURG()) + " = " + "URG (Urgent) <br>";
+    QString ack = "      "  + QString::number(listaPacket[row].getACK()) + " = " + "ACK (Acknowledgement) <br>";
+    QString psh = "      "  + QString::number(listaPacket[row].getPUSH()) + " = " + "PSH (Push) <br>";
+    QString rst = "      "  + QString::number(listaPacket[row].getRST()) + " = " + "RST (Reset) <br>";
+    QString syn = "      "  + QString::number(listaPacket[row].getSYN()) + " = " + "Syn (Syn) <br>";
+    QString fin = "      "  + QString::number(listaPacket[row].getFIN()) + " = " + "Fin (Fin) <br>";
 
     QString packetReconocio = "";
-    qDebug() << "ACUSADO: " << listaPacket[row].getAcusado();
     if( listaPacket[row].getAcusado() != -100 )
-        packetReconocio = "Este paquete reconoce: " + QString::number(listaPacket[row].getAcusado()) + "<br>";
+        packetReconocio = "Este paquete reconoce recepción de fragmento: " + QString::number(listaPacket[row].getAcusado()) + "<br>";
 
     QString segmentData = "";
     if( listaPacket[row].getSizeData() != 0 )
